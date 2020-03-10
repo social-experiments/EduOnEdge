6df485eeacc60274375127726e6750d48b2b397a
Park Swatosh <swatopar@uw.edu>
2020-03-09 21:19:10 -0700

    Added RowEntry.cs

---
+++ World Possible/RowEntry.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace LogToJsonLocal
+{
+
+    // Could be replaced with System.Text.Json most likely
+    // May also be better as a struct
+
+    class RowEntry
+    {
+        [JsonProperty("time_local")]
+        public DateTime TimeLocal { get; set; }
+
+        [JsonProperty("remote_addr")]
+        public string RemoteAddress { get; set; }
+
+        [JsonProperty("remote_user")]
+        public string RemoteUser { get; set; }
+
+        [JsonProperty("request")]
+        public string Request { get; set; }
+
+        public static RowEntry DeserializeJson(string json)
+        {
+            if (string.IsNullOrWhiteSpace(json))
+            {
+                throw new ArgumentException("json can not be null or empty.");
+            }
+
+            return JsonConvert.DeserializeObject<RowEntry>(json);
+        }
+
+        internal string ModuleName()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static RowEntry ParseLogEntryNginx(string entry)
+        {
+            return null;
+        }
+    }
+}

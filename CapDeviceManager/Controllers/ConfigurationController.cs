65f319fc846da3aa16cef99e918ba13bbe1bbffb
Tiona <tionaf@uw.edu>
2020-05-15 18:59:49 -0700

    able to run shell script

--- CapDeviceManager/Controllers/ConfigurationController.cs
+++ CapDeviceManager/Controllers/ConfigurationController.cs
@@ -4,15 +4,27 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using CapDeviceManager.Models;
+using CapDeviceManager.Utils;
 
 namespace CapDeviceManager.Controllers
 {
     public class ConfigurationController : Controller
     {
+        private static String connection;
+
         public IActionResult Index(String connectionString)
         {
+            connection = connectionString;
             var configurationViewModel = new ConfigurationViewModel() { ConnectionString = connectionString };
             return View(configurationViewModel);
+        }
+
+        [HttpPost]
+        public void RunScript()
+        {
+            Console.WriteLine("I'm in  runscript");
+            BashHelper.BashHelloWorld(connection);
+            Console.WriteLine(BashHelper.bashOutput);
         }
     }
 }

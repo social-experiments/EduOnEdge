33151a004b088d1153b4c9eeb89d8d72cd368ff5
amitgalitzky <amit.galitzky@live.com>
2020-06-05 14:37:19 -0700

    gets ip directly from machine

--- CapDeviceManager/Program.cs
+++ CapDeviceManager/Program.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Threading.Tasks;
 using CapDeviceManager.Controllers;
 using CapDeviceManager.Models;
@@ -23,8 +24,25 @@ namespace CapDeviceManager
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();   
-                    //.UseUrls("https://192.168.138.138:5001;http://192.168.138.138:5000");
-                });
+                    NetworkModel result = new NetworkModel(NetworkStatus.Disconnected, "0.0.0.0");
+
+                    // order interfaces by speed and filter out down and loopback
+                    // take first of the remaining
+                    var firstUpInterface = NetworkInterface.GetAllNetworkInterfaces()
+                        .OrderByDescending(c => c.Speed)
+                        .FirstOrDefault(c => c.NetworkInterfaceType != NetworkInterfaceType.Loopback && c.OperationalStatus == OperationalStatus.Up);
+                    if (firstUpInterface != null)
+                    {
+                        var props = firstUpInterface.GetIPProperties();
+                        // get first IPV4 address assigned to this interface
+                        var firstIpV4Address = props.UnicastAddresses
+                            .Where(c => c.Address.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                            .Select(c => c.Address)
+                            .FirstOrDefault();
+
+                        string ip = firstIpV4Address.ToString();
+                        webBuilder.UseStartup<Startup>().UseUrls("https://" +ip +":5001;http://"+ ip +":5000");
+                    }
+                    });
     }
 }
